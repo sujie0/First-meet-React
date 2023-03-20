c1c7865bd02c27e5273139260a938da0f5188216
sujie0 <sujie0@naver.com>
2023-03-21 00:47:32 +0900

    (실습) 시계 만들기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,14 +5,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter_03/Library';
+import Clock from './chapter_04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+setInterval(() => {
 root.render(
   <React.StrictMode>
-    <Library />
+    <Clock />
   </React.StrictMode>
 );
-
+}, 1000); // setInterval 함수를 사용해서 1000msec마다 새롭게 clock component를 root div에 랜더링하도록 만든 코드
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
