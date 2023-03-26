c4b4ea955997d33114cc29d8f88396478cfe06d6
sujie0 <sujie0@naver.com>
2023-03-26 20:23:15 +0900

    (실습) Hooks 사용해보기

---
+++ my-app/src/chapter_07/Accommodate.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import useCounter from "./useCounter";
+
+const MAX_CAPACITY = 10;
+
+function Accommodate(props) {
+    const [isFull, setIsFull]=useState(false);
+    const [count, increaseCount, decreaseCount]=useCounter(0);
+
+    useEffect(() => { //의존성 배열이 없는 형태
+        console.log("======================");
+        console.log("useEffect() is called.");
+        console.log(`isFull: ${isFull}`);
+    });
+
+    useEffect(() => { //의존성 배열이 있는 형태
+        setIsFull(count >= MAX_CAPACITY);
+        console.log(`Current count value: ${count}`);
+    }, [count]);
+
+    return (
+        <div style={{ padding: 16 }}>
+            <p>{`총 ${count}명 수용했습니다.`}</p>
+
+            <button onClick={increaseCount} disabled={isFull}>
+                입장
+            </button>
+            <button onClick={decreaseCount}>퇴장</button>
+
+            {isFull && <p style={{color: "red"}}>정원이 가득찼습니다.</p>}
+        </div>
+    );
+}
+
+export default Accommodate;
